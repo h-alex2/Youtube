30db948c1c9966a7df08c9268dda9205307c8980
h-alex2 <glowhyun1@gmail.com>
2022-07-10 02:36:05 +0900

    🎉 build: new project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "src/Layout";
+
+const App = () => {
+  return (
+    <>
+      <Layout />
+    </>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <App />
+)
