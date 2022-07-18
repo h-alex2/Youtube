3b1e74e1d637e012551bdbdfda5fe31cf0cb7ffa
h-alex2 <glowhyun1@gmail.com>
2022-07-18 23:59:25 +0900

    ✨ feat: add player to watch page

--- src/client/components/watch/PlayerWrapper.jsx
+++ src/client/components/watch/PlayerWrapper.jsx
@@ -1,31 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import styled from 'styled-components'
+import Avatar from '@mui/material/Avatar';
 
-const PlayerWrapper = () => {
+const PlayerWrapper = ({ video }) => {
+  const [channelThumbnail, setChannelThumbnail] = useState("")
+
+  const loadChannelThumbnail = () => {
+    axios.get('api/channel', {
+      params: {
+        channelId: video.channelId,
+      }
+    })
+    .then((res) => setChannelThumbnail(res.data));
+  };
+
+  useEffect(() => {
+    loadChannelThumbnail();
+  }, []);
+
   return (
-    <div className="player">
+    <PlayerContainer>
       <Player
         id="ytplayer"
         type="text/html"
-        width="720"
-        height="405"
-        src="https://www.youtube.com/embed/wCs6M1LSWoc"
+        src={`https://www.youtube.com/embed/${video.id}`}
         frameborder="0"
         allowfullscreen
       />
       <WatchMeta>
-
+        <h3>{video.title}</h3>
+        <hr />
+        <ChannelData>
+          <Avatar
+              className="profile"
+              alt="junghyun-profile"
+              src={channelThumbnail}
+          />
+          <p className='channel__title'>{video.channelTitle}</p>
+        </ChannelData>
+          <p className='description'>{video.description}</p>
       </WatchMeta>
-    </div>
+    </PlayerContainer>
   )
 }
 
-const Player = styled.iframe`
+const PlayerContainer = styled.div`
+  flex: 0.5;
+  padding: 24px 24px 0 0;
+`
 
+const Player = styled.iframe`
+  width: 640px;
+  height: 360px;
+  border: none;
 `
 
 const WatchMeta = styled.div`
+  .description {
+    margin-left: 64px;
+  }
 
+  .channel__title, .description {
+    font-size: 14px;
+  }
+`
+
+const ChannelData = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  .profile {
+    width: 48px;
+    height: 48px;
+    margin-right: 16px;
+  }
 `
 
 export default PlayerWrapper
