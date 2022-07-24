f74fa67acd6d0640adb9462dd814ca515471f2ab
h-alex2 <glowhyun1@gmail.com>
2022-07-24 11:40:03 +0900

    ✨ feat: update

--- src/client/components/watch/RelatedItems.jsx
+++ src/client/components/watch/RelatedItems.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import RelatedItem from './RelatedItem';
+import styled from 'styled-components';
+
 
 const RelatedItems = () => {
   return (
-    <div className="items">RelatedItems</div>
+    <ItemContainer>
+      {videos.map(video => (
+        <RelatedItem video={video} key={video.title} />
+      ))}
+    </ItemContainer>
   )
 }
 
-export default RelatedItems
+const ItemContainer = styled.div`
+  flex: 0.36;
+  display: flex;
+  flex-direction: column;
+  padding: 24px 24px 0 0;
+
+`
+
+export default RelatedItems;
