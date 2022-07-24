fe295afaa65c17065b4a5282468a1ae6d946bd88
h-alex2 <glowhyun1@gmail.com>
2022-07-24 11:39:30 +0900

    ✨ feat: update

--- src/client/components/watch/RelatedItem.jsx
+++ src/client/components/watch/RelatedItem.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { useNavigate } from "react-router-dom";
 
-const RelatedItem = () => {
+const RelatedItem = ({ video }) => {
   return (
-    <div>RelatedItem</div>
+    <Container onClick={onClick}>
+      <Thumbnail src={video.thumbnail} alt="" />
+      <Meta className="content__Info">
+        <p className="title">{video.title}</p>
+        <div className="channel__meta">
+          <p>{video.channelTitle}</p>
+          <p>{video.publishedAt}</p>
+        </div>
+      </Meta>
+    </Container>
   )
 }
 
